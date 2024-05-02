a5b3aa66653c5b093b77a97fac6f6f7308b1c896
L1mir <vladimir.puchynski@gmail.com>
2024-05-02 23:09:13 +0300

    new commit

--- src/main/java/org/limir/springjwtsecurity/JwtCore.java
+++ src/main/java/org/limir/springjwtsecurity/JwtCore.java
@@ -1,7 +1,11 @@
 package org.limir.springjwtsecurity;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+
+import java.util.Date;
 
 @Component
 public class JwtCore {
@@ -9,4 +13,13 @@ public class JwtCore {
     private String secret;
     @Value("${application.security.jwt.expiration}")
     private int lifetime;
+
+    @Deprecated
+    public String generateToken(Authentication authentication) {
+        UserDetailsImpl userDetailsImpl = (UserDetailsImpl)authentication.getPrincipal();
+        return Jwts.builder().setSubject((userDetailsImpl.getUsername())).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + lifetime))
+                .signWith(SignatureAlgorithm.HS256,secret)
+                .compact();
+    }
 }
---
+++ src/main/java/org/limir/springjwtsecurity/User.java
@@ -0,0 +1,20 @@
+package org.limir.springjwtsecurity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String email;
+}
